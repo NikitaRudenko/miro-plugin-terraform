20d56c0561ab448017a8327c35fa7ad51e96d571
anashev <anashev@yamoney.ru>
2020-02-13 12:35:43 +0500

    Add widget content

---
+++ src/scenes/widget-content/widget-content.js
@@ -0,0 +1,11 @@
+miro.onReady(() => {
+	miro.addListener('SELECTION_UPDATED', (e) => {
+		showStatistics(e.data)
+	});
+	miro.board.selection.get().then(showStatistics)
+});
+
+function showStatistics(selection) {
+	console.info('# selection: ', selection);
+}
+
