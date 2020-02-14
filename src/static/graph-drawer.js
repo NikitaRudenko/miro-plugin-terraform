57d9af960a7a2f2d9db9b19fd9c941dd94abd1b2
Nikita Rudenko <nikita.r@miro.com>
2020-02-14 14:07:57 +0500

    add icon detection

--- src/static/graph-drawer.js
+++ src/static/graph-drawer.js
@@ -2,6 +2,25 @@
 
     const METADATA_KEY = '3074457345621854627'
 
+    const NodeType = {
+        /** Mapped to image "Elastic Instance" */
+        AwsLib: 'aws_lb',
+        AwsListener: 'aws_lb_listener',
+
+        /** Mapped to image "EC2 Instance Container" */
+        AwsInstance: 'aws_lb_instance',
+        AwsGroup: 'aws_lb_target_group'
+    }
+
+    const NODE_TYPE_IMAGE_MAP = {
+        [NodeType.AwsLib]: 'elastic-load-balancing_view-light.svg',
+        [NodeType.AwsListener]: 'elastic-load-balancing_view-light.svg',
+        [NodeType.AwsInstance]: 'ec2-instance-container.svg',
+        [NodeType.AwsGroup]: 'ec2-instance-container.svg'
+    }
+
+    const DEFAULT_NODE_TYPE = NodeType.AwsInstance;
+
     class GraphDrawer {
         constructor(graph) {
             this._graph = graph
@@ -10,9 +29,11 @@
 
         _renderNodes() {
             const widgetsToCreate = this._graph.nodes.map((node) => {
+                const imageName = NODE_TYPE_IMAGE_MAP[node.type] || NODE_TYPE_IMAGE_MAP[DEFAULT_NODE_TYPE]
+
                 return {
                     type: 'image',
-                    url: `${static_url}/icons/ec2-instance-container.svg`,
+                    url: `${static_url}/icons/${imageName}`,
                     metadata: {
                         [METADATA_KEY]: {
                             nodeId: node.id
@@ -39,14 +60,7 @@
                 type: 'line',
                 startWidgetId: nodeIdWidgetMap[link.srcNode.id].id,
                 endWidgetId: nodeIdWidgetMap[link.dstNode.id].id
-                // metadata: {
-                //     keyTest: 'valueTest'
-                // },
-                // x: node.x,
-                // y: node.y
             }))
-
-            console.log(links)
 
             return miro.board.widgets.create(links)
         }
@@ -54,7 +68,6 @@
         render() {
             this._renderNodes()
                 .then((nodeIdWidgetMap) => {
-                    console.log(nodeIdWidgetMap)
                     this._renderLinks(nodeIdWidgetMap)
                 })
         }
