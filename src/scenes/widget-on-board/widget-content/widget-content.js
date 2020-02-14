d2e0d8dd8b7e5a845eda8cd3482aee6869a169a9
anashev <anashev@yamoney.ru>
2020-02-14 11:40:26 +0500

    Upload image by each request

--- src/scenes/widget-on-board/widget-content/widget-content.js
+++ src/scenes/widget-on-board/widget-content/widget-content.js
@@ -1,3 +1,8 @@
+const host_url = 'https://86ce345b.ngrok.io';
+
+/** URL to static (icons, public js modules, etc) */
+const static_url = `${host_url}/src/static`;
+
 window.onload = function() {
 	const tPluginDrawControl = document.querySelector('.t-plugin-draw-control')
 	const tPluginEditorControl = document.querySelector('.t-plugin-editor__control')
@@ -11,8 +16,18 @@ window.onload = function() {
 		console.warn(terraformCode)
 
 		apiService.parse(terraformCode)
-			.then((graphs) => {
+			.then((response) => {
 				miro.showNotification('Drawing scheme...');
+
+				return response.graphs;
+			})
+			.then((graphs) => {
+				console.log('graphs: ', graphs);
+
+				miro.board.widgets.create({
+					type: 'image',
+					url: `${static_url}/icons/ec2-instance-container.svg`
+				});
 			})
 			.catch((error) => {
 				console.log('Request failed', error);
@@ -20,6 +35,13 @@ window.onload = function() {
 				miro.showErrorNotification(error.message);
 			})
 	});
+}
+
+/** Graph model */
+class Graph {
+	constructor(params) {
+		this.params = params;
+	}
 }
 
 class APIService {
